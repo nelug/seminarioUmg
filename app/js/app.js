9f1f670679b9d866a8842970565d9baeef6f94a1
Leonel Madrid <nelug@tio-gt.com>
2016-07-17 00:52:27 -0600

    agregando dependencias npm y habilitando gulp

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
-angular.module('seminarioUmg', ['ngRoute', 'ngResource', 'ngMessages', 'ngAnimate']).run(function() {
-
+angular.module('seminarioUmg', ['ngRoute', 'ngResource', 'ngMessages', 'ngAnimate']).run('$scope', '$http', function($scope, $http) {
+  $http.get('/api/persona').success(function(data) {
+      $scope.personas = data;
+  })
 }]);
