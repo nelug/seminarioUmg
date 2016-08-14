523832c3d07a1703a4bd240e2fedc6a805235e43
Leonel Madrid <nelug@tio-gt.com>
2016-08-13 23:03:58 -0600

    agregando color al tema

--- app/js/app.js
+++ app/js/app.js
@@ -1,15 +1,13 @@
 'use strict';
 
-angular.module('seminarioUmg',[ 'ngRoute', 'ngResource', 'ngMessages', 'ngAnimate', 'ngMdIcons', 'ngMaterial', 'md.data.table'])
-.config(function($mdThemingProvider){
+angular.module('seminarioUmg', ['ngRoute', 'ngResource', 'ngMessages', 'ngAnimate', 'ngMdIcons', 'ngMaterial', 'md.data.table'])
+.config( function($mdThemingProvider) {
     $mdThemingProvider.theme('default')
-    .primaryPalette('blue')
-    .accentPalette('brown');
-}).run(function ($rootScope, $location, $route, AuthService) {
-    $rootScope.$on('$routeChangeStart',
-    function (event, next) {
-        AuthService.getUserStatus()
-        .then(function(){
+    .primaryPalette('brown')
+    .accentPalette('blue');
+}).run( function ($rootScope, $location, $route, AuthService) {
+    $rootScope.$on('$routeChangeStart', function (event, next) {
+        AuthService.getUserStatus().then(function() {
             if (next.access.restricted && !AuthService.isLoggedIn()){
                 $location.path('/login');
                 $route.reload();    
