e159a5dc8f214ae31b6aa440744d4e2dbdc14cee
Leonel Madrid <nelug@tio-gt.com>
2016-09-20 14:37:52 -0600

    Agregando error message y mensaje exitoso

--- aplicacion/app/js/controller/venta/crear.js
+++ aplicacion/app/js/controller/venta/crear.js
@@ -1,9 +1,5 @@
 'use strict';
 
-angular.module('seminarioUmg').controller('CrearVentaCtrl', ['$scope', 'toaster', function($scope, toaster) {
+angular.module('seminarioUmg').controller('CrearVentaCtrl', ['$scope', function($scope) {
     $scope.formTitulo = 'Crear Venta';
-    
-    $scope.notificaion = function() {
-        toaster.pop('success', 'Correcto', 'Venta almacenada con exito..');
-    };
 }]);
