39b6d4b033760eb67e8f7aa1171beaef2a76d99c
jorge <jorge@tio-gt.com>
2016-09-20 21:06:57 -0600

    agregando validacion de datos a clientes

--- aplicacion/server/model/cliente.js
+++ aplicacion/server/model/cliente.js
@@ -2,10 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var clienteSchema = new Schema({
-    nit: Number,
-    nombre : String,
-    direccion: String,
-    telefono: Number,
+    nit: {
+    		type: Number,
+            require: true,
+    		validate: [
+    		function(nit){
+    		},
+    		'El nit debe contener solo numeros']
+    	},
+    nombre : {type: String, required: true},
+    direccion: {type: String, required: true},
+    telefono:{type: Number, required: true},
     Usuario: { type: Schema.ObjectId, ref: "User" }
 });
 
