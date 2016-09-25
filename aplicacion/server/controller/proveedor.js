451b01d6a82443dcc324661e7fd0982243f67eb7
jorge <jorge@tio-gt.com>
2016-09-24 20:14:35 -0600

    agregando metodo generico en controlodar en backend a proveedores

--- aplicacion/server/controller/proveedor.js
+++ aplicacion/server/controller/proveedor.js
@@ -10,59 +10,14 @@ exports.getAll = function(req, res) {
 
 
 exports.crear = function(req, res) {
-    Proveedor.create({nit: req.body.nit, empresa: req.body.empresa, representante: req.body.representante,
-        telefono: req.body.telefono, direccion: req.body.direccion},
-        function (err) {
-        if (err) {
-            return res.json({
-                resultado: false,
-                mensaje: error
-            });
-        }
-
-        else {
-            res.json({
-                resultado: true,
-                mensaje: "proveedor almacenado con exito."
-            });
-        }
-    });
+    Controlador.crear(req, res, Proveedor);
 }
 
-exports.editar = function(req, res){
-	Proveedor.update( {_id : req.body._id},
-					{$set:{nit : req.body.nit, empresa: req.body.empresa, representante: req.body.representante,
-                           telefono:req.body.telefono, direccion: req.body.direccion}},
-                    function (err) {
-                    if (err) {
-                        return res.json({
-                            resultado: false,
-                            mensaje: err
-                        });
-                    }
 
-                    else {
-                        res.json({
-                            resultado: true,
-                            mensaje: "Proveedor actualizado con exito."
-                        });
-                    }
+exports.editar = function(req, res) {
+    Controlador.editar(req, res, Proveedor);
+}
 
-			});
-	}
-
-    exports.eliminar = function(req, res){
-        Proveedor.remove( {_id : req.query.id},  function (err) {
-            if (err) {
-                return res.json({
-                    resultado: false,
-                    mensaje: err
-                });
-            } else {
-                res.json({
-                    resultado: true,
-                    mensaje: "Proveedor eliminado con exito."
-                });
-            }
-        });
-    }
+exports.eliminar = function(req, res) {
+    Controlador.eliminar(req, res, Proveedor);
+}
