b94327144903bf71ee6e28c8f1741e5cac0ffbe2
jorge <jorge@tio-gt.com>
2016-09-29 20:28:45 -0600

    resolviendo algunos detalles en productos y ventas.

--- aplicacion/server/model/producto.js
+++ aplicacion/server/model/producto.js
@@ -5,8 +5,9 @@ var productoSchema = new Schema({
     codigo: {type: String, required: 'Codigo debe ser requerido'},
     descripcion : {type: String, required:'Descripcion debe de ser requerida', maxlength: [50, 'la descripcion es demasiado larga']},
     marca : {type: String, required: 'La marca es requerida', maxlength:[25, 'la marca es demasiado larga']},
-    precioCosto: {type: Number, required: 'El precio de venta es requerido'},
-    existencia: {type: Number, required: 'El precio costo es requerido'},
+    precioVenta: {type: Number, required: 'El precio de venta es requerido'},
+    precioCosto: {type: Number, required: 'El precio de costo es requerido'},
+    existencia: {type: Number, required: 'El numero de existencia es requerido'},
     usuario: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
