139e62bf1a2f8ffcdeb13539412911c49bfdb4c0
jorge <jorge@tio-gt.com>
2016-10-03 20:39:35 -0600

    agregando campo de correo a clientes

--- aplicacion/server/model/cliente.js
+++ aplicacion/server/model/cliente.js
@@ -6,6 +6,7 @@ var clienteSchema = new Schema({
     nombre : {type: String, required: 'El nombre es requerdio', maxlength:[50, 'el nomrbre de usuario es demasiado largo']},
     direccion: {type: String, required:'Direccion es requerida', maxlength:[30, 'la direccion es demasiado larga']},
     telefono:{type: Number, required: 'Telefono es requerido y debe ser Numerico'},
+    correo:{type: String, maxlength:[50, 'el correo es invalido o demasiado largo']},
     Usuario: { type: Schema.ObjectId, ref: "User" }
 });
 
