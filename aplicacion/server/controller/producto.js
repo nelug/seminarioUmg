b1261d345aafd7cf0ffde2ca1b7a96c2563d5fec
jorge <jorge@tio-gt.com>
2016-10-03 21:36:39 -0600

    agregando campo existencia minima a productos

--- aplicacion/server/controller/producto.js
+++ aplicacion/server/controller/producto.js
@@ -2,8 +2,8 @@ var Producto = require('../model/producto');
 var Controlador = require('./ControladorBase');
 
 exports.getAll = function(req, res) {
-    Producto.aggregate([         
-        { $project : { _id : 1 , codigo : 1, descripcion: 1, marca: 1, precioVenta: 1,precioCosto: 1, existencia: 1 } }
+    Producto.aggregate([
+        { $project : { _id : 1 , codigo : 1, descripcion: 1, marca: 1, precioCosto: 1,precioVenta: 1, existenciaMin: 1 } }
     ], function(err, producto) {
         res.json(producto);
     });
