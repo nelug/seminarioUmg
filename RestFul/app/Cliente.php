0c7452d59c3c38f59abbbe5436d8540d3f4b9f01
Leonel Madrid <nelug@tio-gt.com>
2016-10-10 21:52:12 -0600

    Agregando relaciones al modelo cliente

--- RestFul/app/Cliente.php
+++ RestFul/app/Cliente.php
@@ -9,4 +9,14 @@ class Cliente extends Model
 	protected $guarded = array('id');
     
     protected $fillable = ['nit', 'nombre', 'direccion', 'telefono', 'correo'];
+    
+    public function ventas()
+    {
+        return $this->hasMany('Venta', 'cliente', 'id');
+    }
+    
+    public function cotizaciones()
+    {
+        return $this->hasMany('Cotizacion', 'cliente', 'id');
+    }
 }
