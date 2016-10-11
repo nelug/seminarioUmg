cd6af49a9029e8e82c9a0986564dff7b5c0d37a3
Leonel Madrid <nelug@tio-gt.com>
2016-10-10 21:54:33 -0600

    Agregando relaciones de productos , proveedores y usuarios

--- RestFul/app/Producto.php
+++ RestFul/app/Producto.php
@@ -9,4 +9,24 @@ class Producto extends Model
 	protected $guarded = array('id');
     
     protected $fillable = ['codigo', 'descripcion', 'precio_venta', 'precio_costo', 'existencia'];
+    
+    public function detalleVenta()
+    {
+        return $this->hasMany('DetalleVenta', 'producto', 'id');
+    }
+    
+    public function detalleCompra()
+    {
+        return $this->hasMany('DetalleCompra', 'producto', 'id');
+    }
+    
+    public function detalleCotizacion()
+    {
+        return $this->hasMany('DetalleCotizacion', 'producto', 'id');
+    }
+    
+    public function detalleDescarga()
+    {
+        return $this->hasMany('DetalleDescarga', 'producto', 'id');
+    }
 }
