f3f0f947c7ec92fd5749498c0673cff219c3c9f1
jorge <jorge@tio-gt.com>
2016-10-11 23:26:24 -0600

    agregando validaciones a clientes, proveedores, productos

--- RestFul/app/Http/Controllers/ClienteController.php
+++ RestFul/app/Http/Controllers/ClienteController.php
@@ -9,59 +9,64 @@ use Illuminate\Support\Facades\DB;
 
 
 class ClienteController extends Controller {
-    
+
     public function obtenerTodos()
     {
         $cliente = Cliente::all();
         return response()->json($cliente);
     }
-    
+
     public function obtenerId($id){
         $data = Cliente::find($id);
         return response()->json($data);
     }
-    
+
     public function crear(Request $request){
-        
+
         $validar = $this->validate($request, [
             'nombre' => 'required',
-            'correo' => 'required'
+            'direccion' => 'required'
         ]);
-        
+
         $data = Cliente::create($request->all());
-        
+
         return response()->json(array(
             'success' => true,
             'mensaje' => 'Cliente almacenado con exito..'
         ));
     }
-    
+
     public function eliminar($id){
         $data = Cliente::whereId($id)->delete();
-        
+
         if (!$data) {
             return response()->json(array(
                 'success' => false,
                 'mensaje' => 'Error'
             ));
         }
-        
+
         return response()->json(array(
             'success' => true,
             'mensaje' => 'Cliente eliminado con exito..'
         ));
     }
-    
+
     public function actualizar(Request $request){
-        $data = Cliente::whereId($request->id)->update($request->all());
-        
-        if (!$data) {
+
+
+        $validar = $this->validate($request, [
+            'nombre' => 'required',
+            'direccion' => 'required'
+        ]);
+        Cliente::whereId($request->id)->update($request->all());
+        /*if (!$data) {
             return response()->json(array(
                 'success' => false,
                 'mensaje' => 'Error'
             ));
-        }
-        
+        }*/
+
         return response()->json(array(
             'success' => true,
             'mensaje' => 'Cliente actualizado con exito..'
--- RestFul/app/Http/Controllers/ProveedorController.php
+++ RestFul/app/Http/Controllers/ProveedorController.php
@@ -9,59 +9,68 @@ use Illuminate\Support\Facades\DB;
 
 
 class ProveedorController extends Controller {
-    
+
     public function obtenerTodos()
     {
-        $cliente = Proveedor::all();
-        return response()->json($cliente);
+        $proveedor = Proveedor::all();
+        return response()->json($proveedor);
     }
-    
+
     public function obtenerId($id){
         $data = Proveedor::find($id);
         return response()->json($data);
     }
-    
+
     public function crear(Request $request){
-        
+
         $validar = $this->validate($request, [
             'nit' => 'required',
-            'empresa' => 'required'
+            'empresa' => 'required',
+            'telefono_empresa' => 'required',
+            'direccion' => 'required'
         ]);
-        
+
         $data = Proveedor::create($request->all());
-        
+
         return response()->json(array(
             'success' => true,
             'mensaje' => 'Proveedor almacenado con exito..'
         ));
     }
-    
+
     public function eliminar($id){
         $data = Proveedor::whereId($id)->delete();
-        
+
         if (!$data) {
             return response()->json(array(
                 'success' => false,
                 'mensaje' => 'Error'
             ));
         }
-        
+
         return response()->json(array(
             'success' => true,
             'mensaje' => 'Proveedor eliminado con exito..'
         ));
     }
-    
+
     public function actualizar(Request $request){
         $data = Proveedor::whereId($request->id)->update($request->all());
-        
+
+        $validar = $this->validate($request, [
+            'nit' => 'required',
+            'empresa' => 'required',
+            'telefono_empresa' => 'required',
+            'direccion' => 'required'
+        ]);
+
         if (!$data) {
             return response()->json(array(
                 'success' => false,
                 'mensaje' => 'Error'
             ));
         }
-        
+
         return response()->json(array(
             'success' => true,
             'mensaje' => 'Proveedor actualizado con exito..'
