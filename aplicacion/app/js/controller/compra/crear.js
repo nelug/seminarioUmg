90ece597b269e15313ed11d050a10bc20058edff
Leonel Madrid <nelug@tio-gt.com>
2016-10-24 17:56:32 -0600

    Creando controlador frontEnd Compra

--- aplicacion/app/js/controller/compra/crear.js
+++ aplicacion/app/js/controller/compra/crear.js
@@ -1,5 +1,29 @@
 'use strict';
 
-angular.module('seminarioUmg').controller('CrearCompraCtrl', [function() {
-    
+angular.module('seminarioUmg').controller('CrearCompraCtrl', ['$scope', '$rootScope', 'ServiceGenericoDetalle',
+function($scope, $rootScope, ServiceGenericoDetalle) {
+    $scope.formTitulo = 'Crear Compra';
+
+    $scope.detalleTabla = [];
+    $scope.formData = {
+        fecha: new Date(),
+        proveedor: [],
+        usuario: [],
+        detalle: []
+    };
+
+    ServiceGenericoDetalle.obtenerProveedoresAC($scope);
+    ServiceGenericoDetalle.obtenerProductosAC($scope);
+    ServiceGenericoDetalle.funcionesCrear($scope, 'Compra');
+
+    $scope.agregarDataDetalle = function() {
+        var dataForm = {
+            cantidad: $scope.dataTemp.cantidad,
+            descripcion: $scope.producto.descripcion,
+            precio:  $scope.dataTemp.precio,
+            producto: $scope.producto.id,
+            total:($scope.dataTemp.cantidad * $scope.producto.precio_venta)
+        };
+        $scope.formData.detalle.push(dataForm);
+    };
 }]);
