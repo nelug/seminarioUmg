9488188923aea73d9d5aaed0b5e7594f32868972
Leonel Madrid <nelug@tio-gt.com>
2016-10-24 22:18:01 -0600

    agregando funcion para eliminar detalle

---
+++ aplicacion/app/js/controller/venta/ventaDetalle.js
@@ -0,0 +1,4 @@
+'use strict';
+
+angular.module('seminarioUmg').controller('ventaDetalleCtrl', [function() {
+}]);
--- aplicacion/app/js/service/ServiceGenericoDetalle.js
+++ aplicacion/app/js/service/ServiceGenericoDetalle.js
@@ -105,6 +105,11 @@ function ($http, $timeout, $q, $log, $route, toaster, jsonPath, $location, $loca
                 toaster.warning('Advertencia.!', jsonPath(data, '$.[0]')[0]);
             });
         };
+
+        $scope.eliminarDetalle = function(item){
+            var index = $scope.formData.detalle.indexOf(item)
+            $scope.formData.detalle.splice(index,1);
+        }
     }
 
 
