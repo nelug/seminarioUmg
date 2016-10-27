a397ea45c7ea5942088153b896eb6f968caeca7e
Leonel Madrid <nelug@tio-gt.com>
2016-10-27 00:51:05 -0600

    agregando dialogo para mostrar detalle de venta

--- aplicacion/app/js/controller/venta/ventaDetalle.js
+++ aplicacion/app/js/controller/venta/ventaDetalle.js
@@ -1,4 +1,8 @@
 'use strict';
 
-angular.module('seminarioUmg').controller('ventaDetalleCtrl', [function() {
+angular.module('seminarioUmg').controller('ventaDetalleCtrl', ['$scope', 'ServiceGenerico', 'data', '$mdDialog',
+function($scope, ServiceGenerico, data, $mdDialog) {
+    $scope.data = data;
+    ServiceGenerico.buscarId($scope, 'detalle-venta');
+    $scope.cancel = function() { $mdDialog.cancel(); };
 }]);
