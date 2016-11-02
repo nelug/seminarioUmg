e361f6b7e1773618c1fd0982e2920224434210b3
Leonel Madrid <nelug@tio-gt.com>
2016-11-01 18:39:48 -0600

    Creando servicio para actualizar permisos

--- aplicacion/app/js/controller/user/tabla.js
+++ aplicacion/app/js/controller/user/tabla.js
@@ -4,10 +4,12 @@ angular.module('seminarioUmg').controller('userTablaCtrl', ['$scope', '$timeout'
 function($scope, $timeout, $http, ServiceGenerico) {
     ServiceGenerico.buscarTodos($scope, 'User');
     ServiceGenerico.instanciarFunciones($scope, 'User');
+    ServiceGenerico.fnPermisosUsuario($scope);
 
     $scope.selectDataTabla = function(dataTabla) {
         $scope.dataSeleccionada = dataTabla;
         $scope.editEnable = true;
         $scope.isUsuario = true;
     };
+
 }]);
