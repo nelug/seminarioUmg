3121b3f3be5d293b180f03ca5f5377f184502ce7
Leonel Madrid <nelug@tio-gt.com>
2016-11-03 19:34:22 -0600

    encriptando password

--- aplicacion/app/js/controller/user/crear.js
+++ aplicacion/app/js/controller/user/crear.js
@@ -2,6 +2,7 @@
 
 angular.module('seminarioUmg').controller('CrearUserCtrl', ['$scope', '$location', 'AuthService', '$mdDialog', 'ServiceGenerico',
 function ($scope, $location, AuthService, $mdDialog, ServiceGenerico) {
+    $scope.encriptPassw = true;
     ServiceGenerico.funcionesDefaultDialog($scope, $mdDialog, 'user');
     $scope.formTitulo = 'Registro de usuario';
 }]);
--- aplicacion/app/js/controller/user/editar.js
+++ aplicacion/app/js/controller/user/editar.js
@@ -2,6 +2,7 @@
 
 angular.module('seminarioUmg').controller('EditarUserCtrl', ['$scope', '$mdDialog', 'ServiceGenerico', 'dataEnviada',
 function ($scope, $mdDialog, ServiceGenerico, dataEnviada) {
+    $scope.encriptPassw = true;
     ServiceGenerico.funcionesDialogoEditar($scope, $mdDialog, dataEnviada, 'user');
     $scope.formTitulo = 'Edicion de usuario';
 }]);
